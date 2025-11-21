3a3cd7a5b2955537ee9d4aa6d8e88315100e3fbc
2ewyeonwoo3 <yeonwooo10607@ewha.ac.kr>
2025-11-21 18:11:58 +0900

    에러 찾기

--- Dstonylion/stonylion/settings.py
+++ Dstonylion/stonylion/settings.py
@@ -49,6 +49,8 @@ REST_FRAMEWORK = {
     ),
 }
 
+CORS_ALLOW_ALL_ORIGINS = True
+
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
