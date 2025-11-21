241bb97bbb6bb471c5067a39cc5d077e941e699d
2ewyeonwoo3 <yeonwooo10607@ewha.ac.kr>
2025-11-21 19:51:18 +0900

    오타 뭐냐

--- config/scripts/deploy.sh
+++ config/scripts/deploy.sh
@@ -42,5 +42,5 @@ install_unidic() {
   docker exec $NAME python3 -m unidic download
 
   docker exec $NAME bash -c "
-    rm -rf /usr/local/lib/python3.10/site-packages/unidic/dicdir &&
-    ln -s /root/.local/share/unidic /usr/local/lib/python3.10/site-packages/unidic/di
+  rm -rf /usr/local/lib/python3.10/site-packages/unidic/dicdir &&
+  ln -s /root/.local/share/unidic /usr/local/lib/python3.10/site-packages/unidic/dicdir"
