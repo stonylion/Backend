c8901cf42d238faad373878f2fdca6d83fd98a40
2ewyeonwoo3 <yeonwooo10607@ewha.ac.kr>
2025-11-26 16:02:46 +0900

    주석처리3

--- config/docker/entrypoint.prod.sh
+++ config/docker/entrypoint.prod.sh
@@ -5,7 +5,7 @@ echo "[ENTRYPOINT] Running Django setup..."
 cd /app/Dstonylion
 
 echo "[ENTRYPOINT] Applying migrations"
-python manage.py migrate --noinput || exit 1
+# python manage.py migrate --noinput || exit 1
 
 echo "[ENTRYPOINT] Collecting static files"
 python manage.py collectstatic --noinput || exit 1
