e7721e9bc49e78a6a031310a0a10da02eef40c67
2ewyeonwoo3 <yeonwooo10607@ewha.ac.kr>
2025-11-26 18:10:14 +0900

    health url 추가

--- Dstonylion/stonylion/urls.py
+++ Dstonylion/stonylion/urls.py
@@ -19,8 +19,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
+
+def health_check(request):
+    return JsonResponse({"status": "ok"}, status=200)
 
 urlpatterns = [
+    path("", health_check),
     path('admin/', admin.site.urls),
     path('api/accounts/', include("accounts.urls")),
     path('api/AI/', include("AI.urls")),
