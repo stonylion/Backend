1de4f3636eddba05233effebb3a087e07df8a6aa
2ewyeonwoo3 <yeonwooo10607@ewha.ac.kr>
2025-11-26 15:53:36 +0900

    migrate 주석처리

--- config/docker/entrypoint.prod.sh
+++ config/docker/entrypoint.prod.sh
@@ -4,10 +4,7 @@ echo "[ENTRYPOINT] Running Django setup..."
 
 cd /app/Dstonylion
 
-echo "[ENTRYPOINT] Fake apply existing AI migration (0005)"
-python manage.py migrate AI 0005_extendchat_extendmessage --fake || true
-
-echo "[ENTRYPOINT] Applying migrations"
+# echo "[ENTRYPOINT] Applying migrations"
 python manage.py migrate --noinput || exit 1
 
 echo "[ENTRYPOINT] Collecting static files"
